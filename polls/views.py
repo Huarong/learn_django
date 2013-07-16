ae6d76458fc28ca02dcd9b69489dfe2da82954c9
huohuarong <huohuarong@gmail.com>
2013-07-16 10:36:39 +0800

    add polls app

---
+++ polls/views.py
@@ -0,0 +1,30 @@
+from django.template import Context, loader
+from polls.models import Poll
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    t = loader.get_template('polls/index.html')
+    c = Context({
+        'latest_poll_list': latest_poll_list,
+        })
+    # output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(t.render(c))
+
+
+def detail(request, poll_id):
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
+    # return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
